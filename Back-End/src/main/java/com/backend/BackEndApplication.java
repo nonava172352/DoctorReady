5e88e4936340172fcec7507748dd42104d7fee7b
Sakon Teerasuwonajak <flukgoza@gmail.com>
2021-11-17 16:41:49 +0700

    starter

--- Back-End/src/main/java/com/backend/BackEndApplication.java
+++ Back-End/src/main/java/com/backend/BackEndApplication.java
@@ -1,4 +1,4 @@
-package com.backend;
+package com.BackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackEndApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackEndApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
 
 }
