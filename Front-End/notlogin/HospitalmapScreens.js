4de3dee6a0fa960d775ee050bba10b9b7cffb6f6
Nontapat Kupreecha <non172352@gmail.com>
2021-11-17 17:04:09 +0700

    17 Nov

--- Front-End/notlogin/HospitalmapScreens.js
+++ Front-End/notlogin/HospitalmapScreens.js
@@ -1,120 +1,101 @@
-import React, {useState, useEffect} from 'react';
-
-// import all the components we are going to use
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
-  Text,
   StyleSheet,
   View,
-  FlatList,
-  TextInput,
+  Platform
 } from 'react-native';
+import MapView, { Heatmap, PROVIDER_GOOGLE } from 'react-native-maps';
 
-const HospitalmapScreens = () => {
-  const [search, setSearch] = useState('');
-  const [filteredDataSource, setFilteredDataSource] = useState([]);
-  const [masterDataSource, setMasterDataSource] = useState([]);
+export default class HospitalmapScreens extends Component {
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(responseJson => {
-        setFilteredDataSource(responseJson);
-        setMasterDataSource(responseJson);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+  static navigationOptions = {
+    title: 'New York',
+  };
 
-  const searchFilterFunction = text => {
-    // Check if searched text is not blank
-    if (text) {
-      // Inserted text is not blank
-      // Filter the masterDataSource and update FilteredDataSource
-      const newData = masterDataSource.filter(function (item) {
-        // Applying filter for the inserted text in search bar
-        const itemData = item.title
-          ? item.title.toUpperCase()
-          : ''.toUpperCase();
-        const textData = text.toUpperCase();
-        return itemData.indexOf(textData) > -1;
-      });
-      setFilteredDataSource(newData);
-      setSearch(text);
-    } else {
-      // Inserted text is blank
-      // Update FilteredDataSource with masterDataSource
-      setFilteredDataSource(masterDataSource);
-      setSearch(text);
+  state = {
+    initialPosition: {
+      latitude: 40.7143,
+      longitude: -74.0042,
+      latitudeDelta: 0.09,
+      longitudeDelta: 0.035
     }
-  };
+  }
 
-  const ItemView = ({item}) => {
+  points = [
+    { latitude: 40.7828, longitude: -74.0065, weight: 1 },
+    { latitude: 41.7121, longitude: -74.0042, weight: 1 },
+    { latitude: 40.7102, longitude: -75.0060, weight: 1 },
+    { latitude: 40.7123, longitude: -74.0052, weight: 1 },
+    { latitude: 40.7032, longitude: -74.0042, weight: 1 },
+    { latitude: 40.7198, longitude: -74.0024, weight: 1 },
+    { latitude: 41.7223, longitude: -74.0053, weight: 1 },
+    { latitude: 40.7181, longitude: -74.0042, weight: 1 },
+    { latitude: 40.7124, longitude: -74.0023, weight: 1 },
+    { latitude: 40.7648, longitude: -74.0012, weight: 1 },
+    { latitude: 41.7128, longitude: -74.0027, weight: 1 },
+    { latitude: 40.7223, longitude: -74.0153, weight: 1 },
+    { latitude: 40.7193, longitude: -74.0052, weight: 1 },
+    { latitude: 40.7241, longitude: -75.0013, weight: 1 },
+    { latitude: 41.7518, longitude: -74.0085, weight: 1 },
+    { latitude: 40.7599, longitude: -74.0093, weight: 1 },
+    { latitude: 41.7523, longitude: -74.0021, weight: 1 },
+    { latitude: 40.7342, longitude: -74.0152, weight: 1 },
+    { latitude: 40.7484, longitude: -75.0042, weight: 1 },
+    { latitude: 40.7929, longitude: -75.0023, weight: 1 },
+    { latitude: 40.7292, longitude: -74.0013, weight: 1 },
+    { latitude: 40.7940, longitude: -74.0048, weight: 1 },
+    { latitude: 40.7874, longitude: -74.0052, weight: 1 },
+    { latitude: 40.7824, longitude: -74.0024, weight: 1 },
+    { latitude: 40.7232, longitude: -74.0094, weight: 1 },
+    { latitude: 41.7342, longitude: -74.0152, weight: 1 },
+    { latitude: 41.7484, longitude: -74.0012, weight: 1 },
+    { latitude: 41.7929, longitude: -74.0073, weight: 1 },
+    { latitude: 41.7292, longitude: -74.0013, weight: 1 },
+    { latitude: 41.7940, longitude: -74.0058, weight: 1 },
+    { latitude: 41.7874, longitude: -74.0352, weight: 1 },
+    { latitude: 41.7824, longitude: -74.0024, weight: 1 },
+    { latitude: 41.7232, longitude: -74.0094, weight: 1 },
+    { latitude: 41.0342, longitude: -75.0152, weight: 1 },
+    { latitude: 41.0484, longitude: -75.0012, weight: 1 },
+    { latitude: 41.0929, longitude: -75.0073, weight: 1 },
+    { latitude: 41.0292, longitude: -74.0013, weight: 1 },
+    { latitude: 41.0940, longitude: -74.0068, weight: 1 },
+    { latitude: 41.0874, longitude: -74.0052, weight: 1 },
+    { latitude: 41.0824, longitude: -74.0024, weight: 1 },
+    { latitude: 41.0232, longitude: -74.0014, weight: 1 }
+  ];
+
+  render() {
     return (
-      // Flat List Item
-      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
-        {item.id}
-        {'.'}
-        {item.title.toUpperCase()}
-      </Text>
-    );
-  };
-
-  const ItemSeparatorView = () => {
-    return (
-      // Flat List Item Separator
-      <View
-        style={{
-          height: 0.5,
-          width: '100%',
-          backgroundColor: '#C8C8C8',
-        }}
-      />
-    );
-  };
-
-  // const getItem = item => {
-  //   // Function for click on an item
-  //   alert('Id : ' + item.id + ' Title : ' + item.title);
-  // };
-
-  return (
-    <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
-        <TextInput
-          style={styles.textInputStyle}
-          onChangeText={text => searchFilterFunction(text)}
-          value={search}
-          underlineColorAndroid="transparent"
-          placeholder="Search Here"
-        />
-        <FlatList
-          data={filteredDataSource}
-          keyExtractor={(item, index) => index.toString()}
-          ItemSeparatorComponent={ItemSeparatorView}
-          renderItem={ItemView}
-        />
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          ref={map => this._map = map}
+          style={styles.map}
+          initialRegion={this.state.initialPosition}>
+          <Heatmap
+            points={this.points}
+            radius={40}
+            opacity={1}
+            gradient={{
+              colors: ["black", "purple", "red", "orange", "white"],
+              startPoints: Platform.OS === 'ios' ? [0.01, 0.04, 0.1, 0.45, 0.5] :
+                [0.1, 0.25, 0.5, 0.75, 1],
+              colorMapSize: 2000
+            }}
+          >
+          </Heatmap>
+        </MapView>
       </View>
-    </SafeAreaView>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    ...StyleSheet.absoluteFillObject
   },
-  itemStyle: {
-    padding: 10,
-  },
-  textInputStyle: {
-    height: 40,
-    borderWidth: 1,
-    paddingLeft: 20,
-    margin: 5,
-    borderColor: '#009688',
-    backgroundColor: '#FFFFFF',
-  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
 });
-
-export default HospitalmapScreens;
