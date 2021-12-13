485a1a5d0ab293a9e92c69f41e1d9bc88c1d87f4
Nontapat Kupreecha <non172352@gmail.com>
2021-12-13 22:43:15 +0700

    13Dec

--- Front-End/alreadylogin/Diseaserecord.js
+++ Front-End/alreadylogin/Diseaserecord.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -10,22 +10,70 @@ import {
   Button,
   TouchableOpacity,
   Alert,
-  ScrollView
+  ScrollView,
+  Platform
 } from 'react-native';
+import DatePicker from 'react-native-datepicker'
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Picker} from '@react-native-picker/picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const Diseaserecord = () => {
   const [username, SetUsername] = React.useState('');
   const [password, SetPassword] = React.useState('');
   const [selectedLanguage, setSelectedLanguage] = useState();
+  const [currentDate, setCurrentData] = useState('');
+  
+  // console.log(currentDate);
 
-  console.log(selectedLanguage);
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const [gettime, setGettime] = useState([]);
+
+  console.log(date)
+
+  console.log(gettime)
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+  useEffect(() => {
+    var date = new Date().getDate(); //Current Date
+    var month = new Date().getMonth() + 1; //Current Month
+    var year = new Date().getFullYear(); //Current Year
+    var hours = new Date().getHours(); //Current Hours
+    var min = new Date().getMinutes(); //Current Minutes
+    var sec = new Date().getSeconds(); //Current Seconds
+    setCurrentData(
+      date + '/' + month + '/' + year + ' ' + hours + ':' + min + ':' + sec
+    );
+  }, [])
   return (
     <SafeAreaView style={styles.container}>
       <Text style={{fontSize: 35, paddingBottom: 20, paddingTop: 20}}>บันทึกอาการ</Text>
       <ScrollView>
+
+        <Text style={{fontSize: 12, marginLeft: '10%'}}>วันที่เเละเวลาปัจจุบัน {currentDate}</Text>  
+
         <Text style={styles.font}>หัวข้ออาการ</Text>
         <TextInput style={styles.input} />
 
@@ -42,6 +90,8 @@ const Diseaserecord = () => {
           <Picker.Item label="มากกว่า 14 วัน" value="มากกว่า 14 วัน" />
         </Picker>
 
+        
+
         <Text style={styles.font}>มีไข้ไหม</Text>
         <TextInput style={styles.input} />
 
@@ -53,12 +103,29 @@ const Diseaserecord = () => {
 
         <Text style={styles.font}>อาการเพิ่มเติม</Text>
         <TextInput style={styles.input} />
+
+        <View>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
       </ScrollView>
       <ScrollView style={{width: 70, marginTop: 12}}>
         <Button
           title="บันทึก"
           color="green"
-          onPress={() => Alert.alert('Simple Button pressed')}
+          onPress={() => setGettime(date)}
         />
       </ScrollView>
     </SafeAreaView>
